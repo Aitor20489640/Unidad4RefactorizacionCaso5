f68c1baf25c30620fd15d2d39c4b7d3dd4d04980
aitgal <aitgal@ubuntu>
2022-04-28 18:46:45 +0200

    Añadidos comentarios a la clase Estudiante para la documentacion

--- Principal/src/desenvolupament/Estudiante.java
+++ Principal/src/desenvolupament/Estudiante.java
@@ -5,11 +5,12 @@
 package desenvolupament;
 
 /**
+ * Clase para crear Estudiantes
  *
  * @author aitgal
  */
 public class Estudiante {
-    
+
     private String dni = "";
     private String nom = "";
     private String cognom = "";
@@ -17,8 +18,8 @@ public class Estudiante {
 
     public Estudiante() {
     }
-    
-    public Estudiante(String dni, String nom, String cognom, int edad){
+
+    public Estudiante(String dni, String nom, String cognom, int edad) {
         this.dni = dni;
         this.nom = nom;
         this.cognom = cognom;
@@ -56,6 +57,5 @@ public class Estudiante {
     public void setEdad(int edad) {
         this.edad = edad;
     }
-    
-    
+
 }
