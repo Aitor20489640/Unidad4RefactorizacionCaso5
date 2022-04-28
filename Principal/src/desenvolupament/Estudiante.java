495d6598029c4ccef8dcb674db692e9bb331bcf5
aitgal <aitgal@ubuntu>
2022-04-28 18:43:43 +0200

    "Hechos cambios en la clase Principal al haber hecho creado la nueva clase Estudiante"

---
+++ Principal/src/desenvolupament/Estudiante.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package desenvolupament;
+
+/**
+ *
+ * @author aitgal
+ */
+public class Estudiante {
+    
+    private String dni = "";
+    private String nom = "";
+    private String cognom = "";
+    private int edad = 5;//Asumimos que la edad minima para matricularse es 5 años
+
+    public Estudiante() {
+    }
+    
+    public Estudiante(String dni, String nom, String cognom, int edad){
+        this.dni = dni;
+        this.nom = nom;
+        this.cognom = cognom;
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getCognom() {
+        return cognom;
+    }
+
+    public void setCognom(String cognom) {
+        this.cognom = cognom;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
--- Principal/src/principal/Principal.java
+++ Principal/src/principal/Principal.java
@@ -20,9 +20,10 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Matricula matricula1 = new Matricula();
+        Matricula matricula1;
         ArrayList<Assignatura> lista = new ArrayList();
         String nomAssig = "Assignatura ";
+        Estudiante persona = new Estudiante("20489640K", "Aitor", "Rodriguez", 20);
         int hores = 3, credits = 5;
         boolean disponible = true;
 
@@ -30,7 +31,7 @@ public class Principal {
             lista.add(new Assignatura(i, nomAssig + i, hores, credits, disponible));
         }
 
-        matricula1 = new Matricula(1, "Aitor", "Rodriguez", "20489640K", 5, lista);
+        matricula1 = new Matricula(1, persona, 5, lista);
 
         System.out.print("Cost de la matricula " + matricula1.costMatricula());
 
